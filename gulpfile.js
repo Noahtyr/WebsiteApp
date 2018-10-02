68b0f2f4a970c70f3d31fc877993c938fd4d7f25
Noahtyr <Noahdavidian@hotmail.dk>
2018-10-02 10:34:20 +0200

    More node modules and gulpfile Changes
    im commiting these changes now, so no issues are gonna happen later

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+
+
+gulp.task('default', defaultTask);
+
+watch = require('gulp-watch');
+
+gulp.task('stream', function () {
+	// Endless stream mode
+	return watch('css/**/*.css', { ignoreInitial: false })
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('callback', function () {
+	// Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+	return watch('css/**/*.css', function () {
+		gulp.src('css/**/*.css')
+			.pipe(gulp.dest('build'));
+	});
+});
+
+function defaultTask(done) {
+  // place code for your default task here
+  done();
+}
